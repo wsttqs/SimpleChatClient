f0e5f4618accc77e7b1a96229fa39cc954828575
wsttqs <dreamlucid@163.com>
2018-11-16 14:09:50 +0800

    优化代码

--- src/Server/ServerGUI.java
+++ src/Server/ServerGUI.java
@@ -1,3 +1,5 @@
+package Server;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -7,11 +9,7 @@ class ServerGUI {
     private Dialog dialog;
     private TextArea textArea;
     private TextField textField;
-    private MenuBar menuBar;
-    private Menu menu;
-    private MenuItem start;
-    private MenuItem loginList;
-    private MenuItem closeServer;
+    private MenuItem onlineNum;
     private MenuItem close;
 
     private ServerGUI() {
@@ -23,6 +21,8 @@ class ServerGUI {
     //GUI初始化
     private void init() {
         //建立并初始化框架和组件
+        MenuBar menuBar;
+        Menu menu;
         frame = new Frame("服务端");
         frame.setLayout(new FlowLayout());
         frame.setBounds(500,300,800,600);
@@ -34,9 +34,7 @@ class ServerGUI {
         sendBut = new Button("发送");
         menuBar = new MenuBar();
         menu = new Menu("菜单");
-        start = new MenuItem("开启服务器");
-        loginList = new MenuItem("已连接用户");
-        closeServer = new MenuItem("关闭服务器");
+        onlineNum = new MenuItem("用户信息");
         close = new MenuItem("退出系统");
 
 
@@ -46,9 +44,7 @@ class ServerGUI {
         frame.add(sendBut);
         frame.setMenuBar(menuBar);
         menuBar.add(menu);
-        menu.add(start);
-        menu.add(loginList);
-        menu.add(closeServer);
+        menu.add(onlineNum);
         menu.add(close);
 
         //开启事件监听
@@ -57,6 +53,10 @@ class ServerGUI {
         //显示框架
         frame.setVisible(true);
         System.out.println("GUI初始化完成");
+        textArea.append("GUI初始化完成"+"\r\n");
+
+        //开启服务器
+        new ServerThread(10002,textArea).start();
     }
     //事件监听方法
     private void frameEvent() {
@@ -67,14 +67,6 @@ class ServerGUI {
                 System.exit(0);
             }
         });
-        //键盘监听：按下ESC键退出程序
-        textField.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode()==KeyEvent.VK_ESCAPE)
-                    System.exit(0);
-            }
-        });
         //键盘监听：按下回车键发送消息
         textField.addKeyListener(new KeyAdapter() {
             @Override
@@ -83,7 +75,7 @@ class ServerGUI {
                     String msg = textField.getText();
                     //如果输入栏没有内容，弹出提示框
                     if (msg.equals(""))
-                        showDialog("发送的消息不能为空");
+                        showDialog("提示：发送的消息不能为空");
                     else {
                         ServerTools.sendMsg("服务器："+msg);
                         textArea.append(msg+"\r\n");
@@ -92,51 +84,47 @@ class ServerGUI {
                 }
             }
         });
-        //Action监听：点击发送按钮发送消息
+        //发送按钮：点击发送消息
         sendBut.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String msg = textField.getText();
                 //如果输入栏没有内容，弹出提示框
                 if (msg.equals(""))
-                    showDialog("发送的消息不能为空");
+                    showDialog("提示：发送的消息不能为空");
                 else {
                     ServerTools.sendMsg("服务器："+msg);
-                    textArea.append(msg+"\r\n");
+                    textArea.append("服务器："+msg+"\r\n");
                     textField.setText("");
                 }
             }
         });
 
-        //Action监听：菜单项对应的处理方式
+        //菜单项对应的处理方式
         ActionListener al = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String s = e.getActionCommand();
-                if (s.equals("开始")) {
-                    new ServerThread(10000,textArea).start();
+                if (s.equals("在线人数")) {
+                    showDialog(ServerTools.getOnlineNum());
                 }
-                if (s.equals("登陆")) {}
-                if (s.equals("登出")) {}
-                if (s.equals("关闭")) {
+                if (s.equals("退出系统")) {
                     System.exit(0);
                 }
             }
         };
 
-        start.addActionListener(al);
-        loginList.addActionListener(al);
-        closeServer.addActionListener(al);
+        onlineNum.addActionListener(al);
         close.addActionListener(al);
     }
-    //弹出对话框方法(可以放init中初始化，这里会慢点)
+    //弹出对话框
     private void showDialog(String msg) {
         //初始化对话框
         dialog = new Dialog(frame,"提示",false);
         dialog.setBounds(780,500,250,130);
         dialog.setLayout(new FlowLayout());
         //创建并初始化对话框中组件
-        Label label = new Label("提示："+msg);
+        Label label = new Label(msg);
         label.setFont(new Font("黑体",Font.PLAIN,20));
         Button okBut = new Button("确定");
 
